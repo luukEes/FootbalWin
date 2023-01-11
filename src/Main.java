ef5d641fa2138597a7760ac463595981162ff02e
Lucas <122219985+luukEes@users.noreply.github.com>
2023-01-11 08:39:26 +0100

    Update Main.java
    Made last update

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
 public class Main {
     public static String footballWin(int AFA, int BFA, int ASB, int BSB) {
-        if ()
+        int multi = BFA + 0.1;
+        int multi2 = ASB + 0.1;
+        String none = "x";
+        String winA = "1";
+        String winB = "2";
+        if (AFA + ASB > BFA + BSB ){
+            return winA;
+        }
+        else if (AFA + ASB < BFA + BSB )  {
+            return winB;
+        }
+        else {
+            return none;
+        }
     }
     public static void main(String[] args) {
+        String result = footballWin(1,2,1,1)
     }
 }
